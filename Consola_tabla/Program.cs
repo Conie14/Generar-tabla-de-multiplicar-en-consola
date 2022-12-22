82bda1aa5eaf40bc1bcdbd32f0ad351391a9772c
Conie14 <44936041+Conie14@users.noreply.github.com>
2022-12-22 13:00:42 -0600

    Genera cnsola de una tabla de multiplicar

---
+++ Consola_tabla/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using libreria_tabla;
+namespace Consola_tabla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("¿De que numero quieres tu tablita:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Tabla algo = new Tabla();
+            algo.p_numero = n;
+            Console.WriteLine(algo.multiplicar());
+            Console.ReadKey();
+        }
+    }
+}
